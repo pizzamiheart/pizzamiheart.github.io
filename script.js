7162d1cc5e1be9604e63f591047ff5d445ac42d8
Andrew Ginn <andrewginn@Andrews-Air.lan>
2024-03-01 22:49:34 -0500

    updates to script.js, main.css, and main.html

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+document.getElementById('redDot').addEventListener('mouseover', function() {
+    document.getElementById('enterBtn').style.transform = 'scale(1)';
+    document.getElementById('enterBtn').style.zIndex = 1;
+});
+
 document.getElementById('enterBtn').addEventListener('click', function() {
-    window.location.href = '/main.html'; // Change to your content page path
+    window.location.href = 'main.html'; // Make sure this matches the name of your main content page
 });
